1df48ecf1ca4469657f587a5e0f4fdd453484098
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-01-26 13:44:28 +0000

    update for Factorio 0.18

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Factorio-Bland-Ore-Colors
 
-Lets you make the base game ores blander colour versions. Useful if you are playing a mixed ore map or just find the ore patches too bright.
+Lets you make the base game ores blander colour versions. Useful if you are playing a mixed ore map or just find the ore patches too bright, i.e. dangOreus
 Colours are based on map color rather than a particular part of the vanilla game's ore image.
 Uranium ore now has no glow.
 
--- data.lua
+++ data.lua
@@ -1,40 +1,39 @@
 local Constants = require("constants")
 
-
 local colorModeSetting = settings.startup["color-mode"].value
 if colorModeSetting == "vanilla" then
     return
 end
 
-
 local oreList = {"iron-ore", "copper-ore", "coal", "stone", "uranium-ore"}
-local oreAlpha = 0.8
 local oresMapColor = {}
 for _, name in pairs(oreList) do
     oresMapColor[name] = data.raw["resource"][name].map_color
-    oresMapColor[name].a = oreAlpha
 end
 
-
-if colorModeSetting == "bland" then
+if colorModeSetting == "dull" then
+    for ore in pairs(oresMapColor) do
+        oresMapColor[ore][4] = 0.8
+    end
+elseif colorModeSetting == "bland" then
     for ore, tint in pairs(oresMapColor) do
-        local difR = (0.5 - tint.r) / 4 * 3
-        oresMapColor[ore].r = tint.r + difR
-        local difG = (0.5 - tint.g) / 4 * 3
-        oresMapColor[ore].g = tint.g + difG
-        local difB = (0.5 - tint.b) / 4 * 3
-        oresMapColor[ore].b = tint.b + difB
+        local difR = (0.5 - tint[1]) / 4 * 3
+        oresMapColor[ore][1] = tint[1] + difR
+        local difG = (0.5 - tint[2]) / 4 * 3
+        oresMapColor[ore][2] = tint[2] + difG
+        local difB = (0.5 - tint[3]) / 4 * 3
+        oresMapColor[ore][3] = tint[3] + difB
+        oresMapColor[ore][4] = 0.8
     end
 elseif colorModeSetting == "grey" then
     for ore in pairs(oresMapColor) do
-        oresMapColor[ore] = {r=0.3, g=0.3, b=0.3, a=0.8}
+        oresMapColor[ore] = {0.3, 0.3, 0.3, 0.8}
     end
 elseif colorModeSetting == "invisible" then
     for ore in pairs(oresMapColor) do
-        oresMapColor[ore] = {r=0, g=0, b=0, a=0}
+        oresMapColor[ore] = {0, 0, 0, 0}
     end
 end
-
 
 for ore, tint in pairs(oresMapColor) do
     local oreProto = data.raw["resource"][ore]
